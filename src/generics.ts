52bdf5e373e197bb379b8338c46b468ee9a22726
galaxxxy <2511030361@qq.com>
2020-03-22 15:26:13 +0800

    范型类与泛型约束

--- src/generics.ts
+++ src/generics.ts
@@ -1,15 +1,34 @@
-function log<T>(value: T): T {
-  console.log(value);
-  return value;
-}
+// function log<T>(value: T): T {
+//   console.log(value);
+//   return value;
+// }
 
-log<string[]>(['a', 'b', 'c']);
-log(['a','v']);
+// log<string[]>(['a', 'b', 'c']);
+// log(['a','v']);
 
 // type Log = <T>(value: T) => T;
 // let myLog: Log = log;
 
-interface Log<T = string> {
-  (value: T): T;
+// interface Log<T = string> {
+  //   (value: T): T;
+  // }
+  // let myLog: Log<number> = log;
+  
+  class Log<T> {
+    run(value: T) {
+      console.log(value);
+      return value;
+    }
+  }
+  let log1 = new Log<number>();
+  log1.run(1);
+let log2 = new Log();
+log2.run({});
+
+interface Length {
+  length: number;
 }
-let myLog: Log<number> = log;
+function log<T extends Length>(value: T): T {
+  console.log(value, value.length);
+  return value;
+}
